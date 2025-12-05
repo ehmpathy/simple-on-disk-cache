806a3b7178afa91da2e24cc1f9e4973e6e00178d
Ulad Kasach <uladkasach@gmail.com>
2025-12-05 06:50:27 -0600

    fix(practs): bump to latest best

--- src/cache.ts
+++ src/cache.ts
@@ -1,7 +1,7 @@
-import { UnexpectedCodePathError } from '@ehmpathy/error-fns';
-import { toMilliseconds, UniDuration } from '@ehmpathy/uni-time';
+import { toMilliseconds, type UniDuration } from '@ehmpathy/uni-time';
 import Bottleneck from 'bottleneck';
 import { promises as fs } from 'fs';
+import { UnexpectedCodePathError } from 'helpful-errors';
 import { createCache as createInMemoryCache } from 'simple-in-memory-cache';
 import { isAFunction, isPresent, withNot } from 'type-fns';
 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export { createCache, SimpleOnDiskCache, DirectoryToPersistTo } from './cache';
+export type { DirectoryToPersistTo, SimpleOnDiskCache } from './cache';
+export { createCache } from './cache';
 export { InvalidOnDiskCacheKeyError } from './key/assertIsValidOnDiskCacheKey';
 export { castToSafeOnDiskCacheKey } from './key/castToSafeOnDiskCacheKey';
